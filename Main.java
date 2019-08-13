db1a10d34225980a5c898b87d0924b9495da65a3
dylan-radilla <dylan_radilla@ciencias.unam.mx>
2019-08-13 11:42:14 -0500

    Practica 1

---
+++ Main.java
@@ -0,0 +1,8 @@
+	public class Main{
+		public static void main(String[]args){
+			System.out.println("hola");
+			System.out.println("openjdk 12.0.2 2019-07-16");
+			System.out.println("OpenJDK Runtime Environment (build 12.0.2+9-Ubuntu-119.04)");
+			System.out.println("OpenJDK 64-Bit Server VM (build 12.0.2+9-Ubuntu-119.04, mixed mode)");
+		}
+	}
