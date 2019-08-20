6508e9c7be1b414d351293f076eeea89784104db
dylan-radilla <dylan_radilla@ciencias.unam.mx>
2019-08-20 11:49:54 -0500

    Practica 3

---
+++ practica03/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+
+	public static void main(String[] args) {
+		int divisor = 5, dividendoEntero = 39;
+		double dividendoReal = 39;
+		System.out.println("dividendoEntero / divisor = " + (dividendoEntero / divisor));
+		System.out.println("dividendoReal / divisor = " + (dividendoReal / divisor));
+	}
+
+}
---
+++ practica03/PrePost.java
@@ -0,0 +1,14 @@
+public class PrePost {
+
+	public static void main(String[] args) {
+		int x = 20;
+		x--;
+		System.out.println(x);
+		--x;
+		System.out.println(x);
+		System.out.println(--x);
+		System.out.println(x--);
+		System.out.println(x);
+	}
+
+}
