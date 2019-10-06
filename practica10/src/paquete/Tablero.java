f971d773c304806ad0d6339ac6d4c8bd79bee018
dylan-radilla <dylan_radilla@ciencias.unam.mx>
2019-10-06 17:06:14 -0500

    Práctica 10

--- practica10/src/paquete/Tablero.java
+++ practica10/src/paquete/Tablero.java
@@ -2,22 +2,44 @@ package paquete;
 
 import processing.core.PApplet;
 
+/**
+ * Programa para dibujar en terminal un tablero de ajedrez.
+ * 
+ * @author Radilla Maldonado Dylan Emmanuel
+ * @version 06/10/2019
+ */
+
 public class Tablero extends PApplet {
 
+	/**
+	 * Método que invoca a PApplet.main(String)
+	 */
 	public static void main(String[] args) {
 		PApplet.main("paquete.Tablero");
 	}
 
+	/**
+	 * Método settings() que establece el tamaño de la terminal en función del largo
+	 * y ancho de la pantalla en que se use.
+	 */
 	@Override
 	public void settings() {
 		size(displayWidth, displayHeight);
 	}
 
+	/**
+	 * Método setup() se ejecuta al iniciar el programa y su función es definir las
+	 * propiedades iniciales del entorno.
+	 */
 	@Override
 	public void setup() {
 
 	}
 
+	/**
+	 * Método draw() que se ejecuta frameRate veces y solo se detiene si se llama
+	 * noLoop() o se cierra el programa.
+	 */
 	@Override
 	public void draw() {
 		for (int i = 0; i < 8; i++) {
